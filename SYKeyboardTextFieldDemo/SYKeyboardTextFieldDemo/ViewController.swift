542cdcefe1cafc9d456a236f9371d9e71f76c18a
余书懿 <441088327@qq.com>
2015-01-29 10:08:14 +0800

    首次提交

---
+++ SYKeyboardTextFieldDemo/SYKeyboardTextFieldDemo/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  SYKeyboardTextFieldDemo
+//
+//  Created by yushuyi on 15/1/29.
+//  Copyright (c) 2015年 yushuyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var keyboardTextField : SYKeyboardTextField!
+    
+    
+    override func loadView() {
+        super.loadView()
+        keyboardTextField = SYKeyboardTextField(point: CGPointMake(0, 100));
+        keyboardTextField.delegate = self
+        keyboardTextField.leftButtonHidden = false
+        keyboardTextField.rightButtonHidden = false
+        keyboardTextField.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleTopMargin
+        self.view.addSubview(keyboardTextField)
+        keyboardTextField.toFullyBottom()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+//MARK: SYKeyboardTextFieldDelegate
+extension ViewController : SYKeyboardTextFieldDelegate {
+    func keyboardTextFieldPressReturnButton(keyboardTextField: SYKeyboardTextField) {
+        UIAlertView(title: "", message: "Action", delegate: nil, cancelButtonTitle: "OK").show()
+    }
+}
